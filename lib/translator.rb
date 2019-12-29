f13051a05892c0b97f12225aae24b575c681fe07
Jorge Santos <neneluis55@gmail.com>
2019-12-29 09:38:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require 'yaml'
 
-def load_library
+def load_library (file_path)
 load_library = YAML.load_file(file_path)
 meaning = {"get_meaning" => {}, "get_emoticon"=> {}}
 
