1574a05bd96fb3e03c371a49c59ddbeefafc15bf
Jorge Santos <neneluis55@gmail.com>
2019-12-29 09:36:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,11 @@ require 'yaml'
 
 def load_library
 load_library = YAML.load_file(file_path)
-meaning = {"get_meaning" => {}, "get_emotion"=> {}}
+meaning = {"get_meaning" => {}, "get_emoticon"=> {}}
 
-load_library.each do |meaning, emotions|
-  meaning["get_meaning"][emotions[1]] = meaning
-  meaning["get_emotion"][emotions[0]] = emotions[1]
+load_library.each do |meaning, emoticons|
+  meaning["get_meaning"][emoticons[1]] = meaning
+  meaning["get_emoticon"][emoticons[0]] = emoticons[1]
 end
 return meaning
 end 
